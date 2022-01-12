2ef9b610ca45c89c725af5b8c21b21f2ed614d64
developer-BOGUM <bogumk1206@gmail.com>
2022-01-12 14:31:00 +0900

    관리자 페이지 개발
    -개발자 김보검

--- src/main/java/egovframework/let/adm/web/AdmController.java
+++ src/main/java/egovframework/let/adm/web/AdmController.java
@@ -4,25 +4,17 @@ import egovframework.com.cmm.ComDefaultVO;
 import egovframework.com.cmm.EgovMessageSource;
 import egovframework.com.cmm.LoginVO;
 import egovframework.com.cmm.util.EgovUserDetailsHelper;
-import egovframework.let.cop.bbs.service.BoardVO;
 import egovframework.let.cop.bbs.service.EgovBBSManageService;
 import org.egovframe.rte.fdl.cmmn.exception.EgovBizException;
-import org.egovframe.rte.ptl.mvc.tags.ui.pagination.PaginationInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import java.util.Map;
-
-import static sun.net.util.IPAddressUtil.checkAuthority;
 
 /**
  * 템플릿 메인 페이지 컨트롤러 클래스(Sample 소스)
@@ -74,8 +66,8 @@ public class AdmController {
 		}
 	}
 
-	@RequestMapping("")
-	public String index(ModelMap model, HttpSession session) throws Exception {
+	@RequestMapping(value = {"/", "/admIndex"})
+	public String admIndex(ModelMap model, HttpSession session) throws Exception {
 		System.out.println("model.toString() = " + model.toString());
 		if (!checkAuthority(model)) {
 			// server-side 권한 확인
@@ -84,7 +76,7 @@ public class AdmController {
 
 		LoginVO loginVO = (LoginVO)session.getAttribute("LoginVO");
 		model.addAttribute("loginVO", loginVO);
-		return "adm/main";
+		return "admIndex";
 
 	}
 }
